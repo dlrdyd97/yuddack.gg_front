d37b8e9220d8ac8403132f3186d3be103e25488a
dlrdyd97 <dlrdyd97@naver.com>
2025-01-12 00:30:15 +0900

    프로필 컴포넌트

---
+++ src/pages/mypage/MyProfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/components/profile/Profile.css";
+
+const MyProfile = () => {
+  return (
+    <>
+      <div className="profile-container">This is a profile message!</div>
+    </>
+  );
+};
+
+export default MyProfile;
