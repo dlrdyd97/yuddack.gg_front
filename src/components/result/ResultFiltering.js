d8b944f345525ab90a9529b35b4f7c119e04b340
HWOwoo <nelap1234@naver.com>
2025-02-12 17:04:18 +0900

    feat : Result > react-select 라이브러리 추가 (npm i 필요)

--- src/components/result/ResultFiltering.js
+++ src/components/result/ResultFiltering.js
@@ -1,31 +1,86 @@
+import React, { useState } from 'react';
+import Select from 'react-select';
+
 function ResultFiltering() {
-  return (
-    <>
-        <div className="result-filtering">
-            <div className="result-filtering-each">
-                <p>매수</p>
-                <select className="result-rare-select" name="sort" id="ea">
-                    <option value="recent">1</option>
-                    <option value="low-price">2</option>
-                    <option value="high-price">3</option>
-                </select>
+
+    const options = [
+        { value: 'recent', label: '최신순' },
+        { value: 'low-price', label: '낮은 가격순' },
+        { value: 'high-price', label: '높은 가격순' }
+    ];
+
+    const eachOptions = [
+        { value: '1', label: '1' },
+        { value: '2', label: '2' },
+        { value: '3', label: '3' }
+    ];
+
+    const [select, setSelect] = useState(options[0]);
+    const [ea, setEa] = useState(eachOptions[0]);
+
+    const customStyles = {
+        control: (provided) => ({
+            ...provided,
+            minHeight: '25px',
+            height: '25px',
+        }),
+        valueContainer: (provided) => ({
+            ...provided,
+            height: '25px',
+            padding: '0 4px',
+        }),
+        input: (provided) => ({
+            ...provided,
+            margin: '0px',
+        }),
+        indicatorsContainer: (provided) => ({
+            ...provided,
+            height: '25px',
+            padding: '0 4px',
+        }),
+        singleValue: (provided) => ({
+            ...provided,
+            fontSize: '11px',
+        }),
+        placeholder: (provided) => ({
+            ...provided,
+            fontSize: '11px',
+        }),
+        menu: (provided) => ({
+            ...provided,
+            fontSize: '11px',
+        }),
+        dropdownIndicator: (provided) => ({
+            ...provided,
+            padding: '0px',
+            fontSize: '11px',
+        }),
+        clearIndicator: (provided) => ({
+            ...provided,
+            padding: '0px',
+            fontSize: '10px',
+        }),
+    };
+
+    return (
+        <>
+            <div className="result-filtering">
+                <div className="result-filtering-each">
+                    <p>매수&nbsp;</p>
+                    <Select options={eachOptions} value={ea} onChange={setEa} styles={customStyles} />
+                </div>
+                <div className="result-filtering-select">
+                    <Select options={options} value={select} onChange={setSelect} styles={customStyles} />
+                </div>
+                <div className="result-filtering-deck">
+                    <input className="result-deck-select" type="checkbox" id="deck" name="deck" value="deck" />
+                    <p>
+                        묶어팔기 제외
+                    </p>
+                </div>
             </div>
-            <div className="result-filtering-select">
-                <select className="result-rare-select" name="sort" id="sort">
-                    <option value="recent">노말</option>
-                    <option value="low-price">슈레</option>
-                    <option value="high-price">울레</option>
-                </select>
-            </div>
-            <div className="result-filtering-deck">
-                <input className="result-deck-select" type="checkbox" id="deck" name="deck" value="deck"/>
-                <p>
-                    묶어팔기 제외
-                </p>
-            </div>
-        </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default ResultFiltering;
--- src/pages/result/Result.js
+++ src/pages/result/Result.js
@@ -1,30 +1,23 @@
 import ResultFiltering from "../../components/result/ResultFiltering";
 import ResultItem from "../../components/result/ResultItem";
+import SearchBar from "../../components/searchBar/SearchBar";
 function Result() {
     return (
       <>
         <div className="ex-container">
-          <div className="ex-header">
-            헤더자리
-          </div>
           <div className="ex-search-bar">
-            검색창 자리
+            <SearchBar/>
           </div>
-
           <div className="result-list">
             <div className="result-header">
               <ResultFiltering/>
             </div>
-
             <ResultItem/>
             <ResultItem/>
             <ResultItem/>
             <ResultItem/>
             <ResultItem/>
-          </div>
-        
-          <div className="ex-footer">
-            푸터자리
+            <div style={{height: "320px"}}>            </div>
           </div>
         </div>
       </>
