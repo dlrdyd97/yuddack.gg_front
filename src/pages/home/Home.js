656706fd531e5aca7a2e55ca2950c3d42c644033
HWOwoo <nelap1234@naver.com>
2025-02-13 17:17:01 +0900

    fix : home 페이지 디자인 수정정

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,22 @@
 import { useNavigate } from "react-router";
+import SearchBar from "../../components/searchBar/SearchBar";
 
 function Home() {
   const navigate = useNavigate();
-
+  const imageUrl = `${process.env.PUBLIC_URL}/images/yuddackgg.png`;
   return (
     <>
-      <div onClick={() => navigate("/mypage/profile")}>마이페이지</div>
-      <div onClick={() => navigate("/result")}>검색결과</div>
-      <div onClick={() => navigate("/registerCard")}>등록</div>
+      <div className="home-main-div">
+      <div className="home-image-div">
+        <img src={imageUrl} alt="Yuddack GG" />
+      </div>
+        <SearchBar />
+      </div>  
+      <div className="home-button-div">
+          <div onClick={() => navigate("/mypage/profile")}>마이페이지</div>
+          <div onClick={() => navigate("/result")}>검색결과</div>
+          <div onClick={() => navigate("/registerCard")}>등록</div>
+        </div>
     </>
   );
 }
