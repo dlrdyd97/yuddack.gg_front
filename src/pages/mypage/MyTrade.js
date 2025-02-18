33df5f5dbee425428cfbc33858182aa6be3d402e
dlrdyd97 <dlrdyd97@naver.com>
2025-02-18 20:03:38 +0900

    fix : mypage 페이지 디자인 수정

--- src/pages/mypage/MyTrade.js
+++ src/pages/mypage/MyTrade.js
@@ -13,47 +13,63 @@ const Mytrade = ({ trade, userInfo, setUserInfo }) => {
   return (
     <>
       <div className="trade-box">
-        <>
-          {trade.option === "single" ? (
+        <div className="trade-top">{trade.title}</div>
+        <div className="trade-image-container">
+          <img src={trade.post_picture} alt="Profile" className="trade-image" />
+        </div>
+        {trade.option === "single" ? (
+          <>
+            <div className="trade-bottom">
+              {/* 특수문자 '[ ]' 가 문자보다 아래 출력되는 현상이 있어서 style 추가 */}
+              <span className="trade-special-char">[</span>
+              <span>{trade.quantity}장</span>
+              <span className="trade-special-char">]&nbsp;</span>
+              <span>
+                개당 {new Intl.NumberFormat("ko-KR").format(trade.price)}
+                원&nbsp;
+              </span>
+            </div>
+            <div className="trade-option">
+              <div className="trade-option-single">낱장</div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="trade-bottom">
+              <span>
+                {new Intl.NumberFormat("ko-KR").format(trade.price)}원
+              </span>
+            </div>
+            <div className="trade-option">
+              <div className="trade-option-deck">덱</div>
+            </div>
+          </>
+        )}
+        {/* 부가 옵션 */}
+        <div className="trade-extraOptions-box">
+          {/* 흥정 여부 */}
+          {trade.negotiable === 1 ? (
             <>
-              <div className="trade-top">{trade.title}</div>
-              <div className="trade-bottom">
-                {/* 특수문자 '[ ]' 가 문자보다 아래 출력되는 현상이 있어서 style 추가 */}
-                <span className="trade-special-char">[</span>
-                <span>{trade.quantity}장</span>
-                <span className="trade-special-char">]&nbsp;</span>
-                <span>
-                  개당 {new Intl.NumberFormat("ko-KR").format(trade.price)}
-                  원&nbsp;
-                </span>
-                {trade.rarity === "" || trade.rarity === "상관없음" ? (
-                  <></>
-                ) : (
-                  <>
-                    <span className="trade-extraOptions">{trade.rarity}</span>
-                  </>
-                )}
-              </div>
-              <div className="trade-option">
-                <div className="trade-option-single">낱장</div>
-              </div>
+              <span className="trade-extraOptions-possible">흥정 가능</span>
             </>
           ) : (
             <>
-              <div className="trade-top">{trade.title}</div>
-              <div className="trade-bottom">
-                <span>
-                  {new Intl.NumberFormat("ko-KR").format(trade.price)}원
-                </span>
-              </div>
-              <div className="trade-option">
-                <div className="trade-option-deck">덱</div>
-              </div>
+              <span className="trade-extraOptions-impossible">흥정 불가</span>
             </>
           )}
-
-          <div className="trade-date">{getDaysAgo(trade.date)}일 전</div>
-        </>
+          {/* 레어도 */}
+          {trade.rarity === "" || trade.rarity === "상관없음" ? (
+            <></>
+          ) : (
+            <>
+              <span className="trade-extraOptions-rarity">{trade.rarity}</span>
+            </>
+          )}
+        </div>
+        {/*  몇 분 , 몇  시간 , 며칠 전 수정 */}
+        <div className="trade-date">{getDaysAgo(trade.date)}일 전</div>
+        {/* 코맨트 */}
+        <div className="trade-comment">{trade.comment}</div>
       </div>
     </>
   );
